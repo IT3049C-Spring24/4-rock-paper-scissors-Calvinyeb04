056af0983718ac781ee4a439e39ae11448805e5f
Calvin Yeboah <113629722+Calvinyeb04@users.noreply.github.com>
2024-01-30 05:20:49 -0500

    completed  the index file

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,42 +1,41 @@
 // Elements
 const welcomeScreen = document.getElementById(`welcome-screen`);
-const gameScreen = ;
-const startGameButton = ;
-const userNameInput = ;
-const userSelection = ;
-const goButton = ;
-const scoreParagraph = ;
-const gameHistoryParagraph = ;
-
-// instantiate the game object from the `RockPaperScissors` class.
-let game;
+const gameScreen = document.getElementById(`game-screen`);
+const startGameButton = document.getElementById(`start-game-button`);
+const userNameInput = document.getElementById(`username`);
+const userSelection = document.getElementById(`user-selection`);
+const goButton = document.getElementById(`go-button`);
+const scoreParagraph = document.getElementById(`score`);
+const gameHistoryParagraph = document.getElementById(`game-history`);
 
 // hide game screen
 gameScreen.classList.add(`d-none`);
 
 // updateScoreTallyUI
-function updateScoreTallyUI(){
-
+function updateScoreTallyUI() {
+  scoreParagraph.textContent = `${game.username}: ${game.score.user} v CPU: ${game.score.cpu}`;
 }
 
 // updateGameHistoryUI
-function updateGameHistoryUI(){
-
+function updateGameHistoryUI() {
+  gameHistoryParagraph.textContent = game.gameHistoryLog.join(", ");
 }
 
 // start-game-button EventListener
 startGameButton.addEventListener(`click`, function (e) {
-  const username = 
-  game = new RockPaperScissors(userName);
-  // Complete
+  e.preventDefault();
+  const username = userNameInput.value;
+  game = new RockPaperScissors(username);
+  welcomeScreen.classList.add(`d-none`);
+  gameScreen.classList.remove(`d-none`);
+  updateScoreTallyUI();
 });
 
 // go-button EventListener
 goButton.addEventListener(`click`, function (e) {
   e.preventDefault();
+  const userSelectionValue = userSelection.value;
+  const result = game.play(userSelectionValue);
+  updateScoreTallyUI();
+  updateGameHistoryUI();
 });
-
-// If you're doing the extra-credit, uncomment the below: reset-game-button
-// resetGameButton.addEventListener(`click`, function(e) { 
-  
-// });
